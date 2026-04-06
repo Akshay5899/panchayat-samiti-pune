0f319a7f0cb3498748a090d972a0350634a8d7eb
sagar jadhav <sagar.jadhav@brawizz.com>
2026-04-06 18:13:59 +0530

    some changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,128 +1,147 @@
+import { useEffect } from "react"
+import * as bootstrap from "bootstrap"
 import "./hero.css"
 
 function Hero() {
+
+  useEffect(() => {
+    const el = document.querySelector('#heroCarousel')
+
+    if (el) {
+      new bootstrap.Carousel(el, {
+        interval: 5000,
+        ride: 'carousel',
+        pause: false,
+        wrap: true
+      })
+    }
+  }, [])
+
   return (
     <section className="hero-section">
 
       <div className="container-fluid sec">
         <div className="row g-0">
 
-<div className="col-sm-8 hero-left">
+          {/* LEFT SLIDER */}
+          <div className="col-sm-8 hero-left">
 
-  <div id="heroCarousel" className="carousel slide" data-bs-ride="carousel" data-bs-interval="3000" data-bs-pause="false">
+            <div
+              id="heroCarousel"
+              className="carousel slide carousel-fade"
+            >
 
-    <div className="carousel-inner">
+              <div className="carousel-inner">
 
-      {/* SLIDE 1 */}
-      <div className="carousel-item active">
-        <div className="hero-overlay">
-          <h1 className="hero-title">
-            ओळख पुण्याची, शान महाराष्ट्राची !
-          </h1>
-          <p className="hero-subtitle">
-            देश विदेशातील पाहुण्यांचे हार्दिक स्वागत
-          </p>
-          <div className="hero-buttons">
-            <button className="btn all-org btn-warning me-3">
-              महत्त्वाच्या योजना
-            </button>
-            <button className="btn btn-light">
-              संपूर्ण सूचना
-            </button>
+                <div className="carousel-item active slide1">
+                  <div className="hero-overlay">
+                    <h1 className="hero-title">
+                      ओळख पुण्याची, शान महाराष्ट्राची !
+                    </h1>
+                    <p className="hero-subtitle">
+                      देश विदेशातील पाहुण्यांचे हार्दिक स्वागत
+                    </p>
+                    <div className="hero-buttons">
+                      <a href="#" target="blank"><button className="btn btn-warning me-3">
+                        महत्त्वाच्या योजना
+                      </button></a>
+                      <a href="#" target="blank"><button className="btn btn-light">
+                        संपूर्ण सूचना
+                      </button></a>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="carousel-item slide2">
+                  <div className="hero-overlay">
+                    <h1 className="hero-title">
+                      ओळख पुण्याची, शान महाराष्ट्राची !
+                    </h1>
+                    <p className="hero-subtitle">
+                      देश विदेशातील पाहुण्यांचे हार्दिक स्वागत
+                    </p>
+                    <div className="hero-buttons">
+                      <a href="#" target="blank"><button className="btn btn-warning me-3">
+                        महत्त्वाच्या योजना
+                      </button></a>
+                      <a href="#" target="blank"><button className="btn btn-light">
+                        संपूर्ण सूचना
+                      </button></a>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="carousel-item slide3">
+                  <div className="hero-overlay">
+                    <h1 className="hero-title">
+                      ओळख पुण्याची, शान महाराष्ट्राची !
+                    </h1>
+                    <p className="hero-subtitle">
+                      देश विदेशातील पाहुण्यांचे हार्दिक स्वागत
+                    </p>
+                    <div className="hero-buttons">
+                      <a href="#" target="blank"><button className="btn btn-warning me-3">
+                        महत्त्वाच्या योजना
+                      </button></a>
+                      <a href="#" target="blank"><button className="btn btn-light">
+                        संपूर्ण सूचना
+                      </button></a>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="carousel-item slide4">
+                  <div className="hero-overlay">
+                    <h1 className="hero-title">
+                      ओळख पुण्याची, शान महाराष्ट्राची !
+                    </h1>
+                    <p className="hero-subtitle">
+                      देश विदेशातील पाहुण्यांचे हार्दिक स्वागत
+                    </p>
+                    <div className="hero-buttons">
+                      <a href="#" target="blank"><button className="btn btn-warning me-3">
+                        महत्त्वाच्या योजना
+                      </button></a>
+                      <a href="#" target="blank"><button className="btn btn-light">
+                        संपूर्ण सूचना
+                      </button></a>
+                    </div>
+                  </div>
+                </div>
+
+              </div>
+
+              {/* CONTROLS */}
+              <button
+                className="carousel-control-prev"
+                type="button"
+                data-bs-target="#heroCarousel"
+                data-bs-slide="prev"
+              >
+                <span className="carousel-control-prev-icon"></span>
+              </button>
+
+              <button
+                className="carousel-control-next"
+                type="button"
+                data-bs-target="#heroCarousel"
+                data-bs-slide="next"
+              >
+                <span className="carousel-control-next-icon"></span>
+              </button>
+
+            </div>
           </div>
-        </div>
-      </div>
 
-      {/* SLIDE 2 */}
-      <div className="carousel-item">
-        <div className="hero-overlay">
-          <h1 className="hero-title">
-            ओळख पुण्याची, शान महाराष्ट्राची !
-          </h1>
-          <p className="hero-subtitle">
-            देश विदेशातील पाहुण्यांचे हार्दिक स्वागत
-          </p>
-          <div className="hero-buttons">
-            <button className="btn all-org btn-warning me-3">
-              महत्त्वाच्या योजना
-            </button>
-            <button className="btn btn-light">
-              संपूर्ण सूचना
-            </button>
-          </div>
-        </div>
-      </div>
-
-      {/* SLIDE 3 */}
-      <div className="carousel-item">
-        <div className="hero-overlay">
-          <h1 className="hero-title">
-            ओळख पुण्याची, शान महाराष्ट्राची !
-          </h1>
-          <p className="hero-subtitle">
-            देश विदेशातील पाहुण्यांचे हार्दिक स्वागत
-          </p>
-          <div className="hero-buttons">
-            <button className="btn all-org btn-warning me-3">
-              महत्त्वाच्या योजना
-            </button>
-            <button className="btn btn-light">
-              संपूर्ण सूचना
-            </button>
-          </div>
-        </div>
-
-      </div>
-
-      {/* SLIDE 4 */}
-      <div className="carousel-item">
-        <div className="hero-overlay">
-          <h1 className="hero-title">
-            ओळख पुण्याची, शान महाराष्ट्राची !
-          </h1>
-          <p className="hero-subtitle">
-            देश विदेशातील पाहुण्यांचे हार्दिक स्वागत
-          </p>
-          <div className="hero-buttons">
-            <button className="btn all-org btn-warning me-3">
-              महत्त्वाच्या योजना
-            </button>
-            <button className="btn btn-light">
-              संपूर्ण सूचना
-            </button>
-          </div>
-        </div>
-      </div>
-
-    </div>
-
-    {/* CONTROLS */}
-    <button className="carousel-control-prev" type="button" data-bs-target="#heroCarousel" data-bs-slide="prev">
-      <span className="carousel-control-prev-icon"></span>
-    </button>
-
-    <button className="carousel-control-next" type="button" data-bs-target="#heroCarousel" data-bs-slide="next">
-      <span className="carousel-control-next-icon"></span>
-    </button>
-
-  </div>
-
-</div>
-
-
-          {/* RIGHT SIDE CARD */}
+          {/* RIGHT SIDE */}
           <div className="col-sm-4 hero-right">
-
             <div className="info-card">
-
               <img
                 src="/images/election.png"
                 className="img-fluid"
                 alt="Election"
               />
-
             </div>
-
           </div>
 
         </div>
