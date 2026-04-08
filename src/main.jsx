f948bb7c8378ee9d0177e2ffaae84f6f7b611b40
sagar jadhav <sagar.jadhav@brawizz.com>
2026-04-08 11:06:36 +0530

    nav menu

--- src/main.jsx
+++ src/main.jsx
@@ -9,9 +9,7 @@ import 'bootstrap-icons/font/bootstrap-icons.css'
 import { BrowserRouter } from "react-router-dom"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
 )
